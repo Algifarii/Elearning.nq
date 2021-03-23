a6951cf9631aa9de5024aa397d7de7caa405456d
abudzar300 <abudzar300@gmail.com>
2021-03-23 10:26:14 +0700

    v.1.0

---
+++ elearning/config/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$host	= "localhost";
+$user	= "root";
+$pass	= "";
+$db	= "elarning10jun2020";
+$mysqli = new mysqli($host, $user, $pass, $db);
+date_default_timezone_set('Asia/Jakarta');
+?>
